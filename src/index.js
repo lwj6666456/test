c212c134a011eeac6d676e45d5a535cedece9de0
lwj6666456 <a313908900>
2019-03-07 20:52:06 +0800

    存档

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 import util from './util'
-import { fn1, fn2 } from './util2'
-console.log(util.a)
-fn1()
-fn2()
+import {
+    fn1,
+    fn2
+} from './util2'
+
+function loadImg(src) {
+    const promise = new Promise((resolve, reject) => {
+        try {
+            var img = document.createElement('img')
+            img.onload = function () {
+                resolve("成功")
+            }
+            img.onerror = function () {
+                resolve("失败")
+            }
+            img.src = src
+            
+        } catch (err) {
+            reject(err)
+        }
+
+    })
+    return promise
+}
+var src = 'www.baidu.com/tt.jpg'
+loadImg(src).then(res => {
+    console.log(res)
+}, err => {
+    console.error(err)
+})
--- webpack.conf.js
+++ webpack.conf.js
@@ -1,13 +1,16 @@
 const path = require('path')
 const HTMLPlugin = require('html-webpack-plugin')
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: {
-        app: path.join(__dirname, './src/index.js')
+        app: path.join(__dirname, './src/index.js'),
+    },
+    resolve: {
+        extensions: ['.jsx', '.js']
     },
     output: {
         path: path.join(__dirname, './dist'),
-        filename: '[name].[hash].js'
+        filename: '[name].[hash].js',
     },
     module: {
         rules: [{
@@ -21,5 +24,17 @@ module.exports = {
         new HTMLPlugin({
             template: path.join(__dirname, './src/index.html')
         })
-    ]
+    ],
+    devServer: {
+        host: '0.0.0.0',
+        port: '8888',
+        contentBase: path.join(__dirname, './dist'),
+        hot: true,
+        overlay: {
+            errors: true // 错误信息 黑框
+        },
+        historyApiFallback: {
+            index: 'index.html'
+        }
+    }
 }
